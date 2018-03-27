cfb0716b6c54272d8bdb60144f2f2fcd6655ec89
Kelsey Judson <kelseyjudson@protonmail.com>
2018-03-27 17:42:50 +1300

    Value object specs

---
+++ spec/lucid_shopify/result_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'lucid_shopify/result'
+
+RSpec.describe LucidShopify::Result do
+  context 'without error' do
+    subject(:result) { LucidShopify::Result.new('foo') }
+
+    it { is_expected.to have_attributes(value: 'foo') }
+    it { is_expected.to have_attributes(error: nil) }
+    it { is_expected.to satisfy('succeed', &:success?) }
+    it { is_expected.not_to satisfy('fail', &:failure?) }
+  end
+
+  context 'with error' do
+    subject(:result) { LucidShopify::Result.new('foo', 'bar') }
+    
+    it { is_expected.to have_attributes(value: 'foo') }
+    it { is_expected.to have_attributes(error: 'bar') }
+    it { is_expected.not_to satisfy('succeed', &:success?) }
+    it { is_expected.to satisfy('fail', &:failure?) }
+  end
+end
---
+++ spec/lucid_shopify/webhook_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'lucid_shopify/webhook'
+
+RSpec.describe LucidShopify::Webhook do
+  let(:myshopify_domain) { 'example.myshopify.com' }
+  let(:topic) { 'shop/update' }
+  let(:data) { '{"name": "Example"}' }
+  
+  subject(:webhook) { LucidShopify::Webhook.new(myshopify_domain, topic, data) }
+
+  it { is_expected.to have_attributes(myshopify_domain: myshopify_domain) }
+  it { is_expected.to have_attributes(topic: topic) }
+  it { is_expected.to have_attributes(data: data) }
+  it { is_expected.to have_attributes(data_hash: {'name' => 'Example'}) }
+
+  context 'with invalid JSON' do
+    let(:data) { 'plain text' }
+
+    it { is_expected.to have_attributes(data: data) }
+    it { is_expected.to have_attributes(data_hash: {}) }
+  end
+end
