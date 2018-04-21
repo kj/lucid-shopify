c0ceda1b22ae4c51834931dde0413f4e04fc23e6
Kelsey Judson <kelseyjudson@protonmail.com>
2018-04-22 01:29:29 +1200

    Some more config options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,12 @@ require_relative 'support/fixture_helpers'
 require_relative 'support/matchers'
 
 RSpec.configure do |config|
+  config.disable_monkey_patching!
   config.expose_dsl_globally = false
+  config.filter_gems_from_backtrace 'dry-container', 'dry-initializer', 'http'
   config.include(CredentialsHelpers)
   config.include(FixtureHelpers)
+  # config.order = :random # TODO: use order: :defined metadata for specific specs
 end
 
 require 'lucid_shopify/config'
