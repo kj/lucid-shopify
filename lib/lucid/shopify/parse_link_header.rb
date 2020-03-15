80223a842fb661c8ac039593305491a4921172df
Kelsey Judson <kelseyjudson@protonmail.com>
2020-03-14 19:02:58 -0500

    A weird header quirk

--- lib/lucid/shopify/parse_link_header.rb
+++ lib/lucid/shopify/parse_link_header.rb
@@ -12,7 +12,24 @@ module Lucid
       #
       # @return [Hash]
       def call(link_header)
-        return {} if link_header.nil?
+        case link_header
+        # NOTE: There is a strange behaviour where it seems that if the header
+        # value exceeds a certain length, it is split into chunks. It seems that
+        # if you use {HTTP::Headers#get}, you will always get {Array<String},
+        # and it is the special behaviour of {HTTP::Headers#[]} causing this.
+        #
+        # However, why is it split in the first place? Does Shopify send
+        # multiple Link headers (as chunks) for longer values? There does not
+        # seem to be any limit on the length of value in the HTTP specification.
+        #
+        # https://www.rubydoc.info/gems/http/HTTP/Headers#[]-instance_method
+        # https://www.rubydoc.info/gems/http/HTTP/Headers#get-instance_method
+        when Array
+          link_header = link_header.join
+        when String
+        else
+          return {}
+        end
 
         link_header.split(',').map do |link|
           url, rel = link.split(';') # rel should be the first param
